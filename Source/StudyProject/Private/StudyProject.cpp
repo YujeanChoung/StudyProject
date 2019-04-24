b27fecee92ba189b4e88bce916cf3e4dd611baa8
yjchoung <dyjchoung@naver.com>
2019-04-24 10:58:20 +0900

    Move files to Public and Private Folders

---
+++ Source/StudyProject/Private/StudyProject.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "StudyProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StudyProject, "StudyProject" );
+ 
